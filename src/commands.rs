35b329fd2b1213a8dcd15d5c95e20604c6dc7e97
snowdog322 <ryba@ryba.pl>
2024-11-22 20:17:50 +0100

    added commands mod

---
+++ src/commands.rs
@@ -0,0 +1,6 @@
+pub mod commands{
+    pub fn main(){
+        println!("sigma");
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod graphics;
+mod commands;
 
 use graphics::graphics as graphicsmod;
+use commands::commands as commandsmod;
 
 
 fn main() {
     graphicsmod::main();
+    commandsmod::main();
 }
